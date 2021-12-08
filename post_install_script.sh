ce3f0d970833ddb6337c9f66933f73f17d7a4296
nikhilc149 <nikhilchaudhari149@gmail.com>
2021-12-08 21:09:23 +0530

    Add rootCA.pem and control_proxy.yml file

--- post_install_script.sh
+++ post_install_script.sh
@@ -2,62 +2,55 @@
 
 CONTROL_PROXY_FILE_PATH="/var/opt/magma/configs/control_proxy.yml"
 
-create_control_proxy_yml_file()
-{
-    mkdir -p /var/opt/magma/configs/ 
-    > $CONTROL_PROXY_FILE_PATH
-    echo "Creating control_proxy.yml file"
-    echo "
-local_port: $local_port
-cloud_address: $cloud_address
-cloud_port: $cloud_port
+ROOT_CA_PEM_FILE_PATH="/var/opt/magma/certs/rootCA.pem"
 
-bootstrap_address: $bootstrap_address
-bootstrap_port: $bootstrap_port
+mkdir -p /var/opt/magma/configs/ 
+> $CONTROL_PROXY_FILE_PATH
 
-fluentd_address: $fluentd_address
-fluentd_port: $fluentd_port
+echo "Creating control_proxy.yml file"
+echo "cloud_address: controller.dev-gxcnetwork.com
+cloud_port: 443
+
+bootstrap_address: bootstrapper-controller.dev-gxcnetwork.com
+bootstrap_port: 443
+
+fluentd_address: fluentd.dev-gxcnetwork.com
+fluentd_port: 24224
 
 rootca_cert: /var/opt/magma/tmp/certs/rootCA.pem
 gateway_cert: /var/opt/magma/certs/gateway.crt
 gateway_key: /var/opt/magma/certs/gateway.key" >> $CONTROL_PROXY_FILE_PATH 
 
-}
+mkdir -p /var/opt/magma/certs/ 
+> $ROOT_CA_PEM_FILE_PATH
+
+echo "Creating rootCA.pem file"
+echo "-----BEGIN CERTIFICATE-----
+MIIDxTCCAq2gAwIBAgIQAqxcJmoLQJuPC3nyrkYldzANBgkqhkiG9w0BAQUFADBsMQswCQYDVQQG
+EwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cuZGlnaWNlcnQuY29tMSsw
+KQYDVQQDEyJEaWdpQ2VydCBIaWdoIEFzc3VyYW5jZSBFViBSb290IENBMB4XDTA2MTExMDAwMDAw
+MFoXDTMxMTExMDAwMDAwMFowbDELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZ
+MBcGA1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTErMCkGA1UEAxMiRGlnaUNlcnQgSGlnaCBBc3N1cmFu
+Y2UgRVYgUm9vdCBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMbM5XPm+9S75S0t
+Mqbf5YE/yc0lSbZxKsPVlDRnogocsF9ppkCxxLeyj9CYpKlBWTrT3JTWPNt0OKRKzE0lgvdKpVMS
+OO7zSW1xkX5jtqumX8OkhPhPYlG++MXs2ziS4wblCJEMxChBVfvLWokVfnHoNb9Ncgk9vjo4UFt3
+MRuNs8ckRZqnrG0AFFoEt7oT61EKmEFBIk5lYYeBQVCmeVyJ3hlKV9Uu5l0cUyx+mM0aBhakaHPQ
+NAQTXKFx01p8VdteZOE3hzBWBOURtCmAEvF5OYiiAhF8J2a3iLd48soKqDirCmTCv2ZdlYTBoSUe
+h10aUAsgEsxBu24LUTi4S8sCAwEAAaNjMGEwDgYDVR0PAQH/BAQDAgGGMA8GA1UdEwEB/wQFMAMB
+Af8wHQYDVR0OBBYEFLE+w2kD+L9HAdSYJhoIAu9jZCvDMB8GA1UdIwQYMBaAFLE+w2kD+L9HAdSY
+JhoIAu9jZCvDMA0GCSqGSIb3DQEBBQUAA4IBAQAcGgaX3NecnzyIZgYIVyHbIUf4KmeqvxgydkAQ
+V8GK83rZEWWONfqe/EW1ntlMMUu4kehDLI6zeM7b41N5cdblIZQB2lWHmiRk9opmzN6cN82oNLFp
+myPInngiK3BD41VHMWEZ71jFhS9OMPagMRYjyOfiZRYzy78aG6A9+MpeizGLYAiJLQwGXFK3xPkK
+mNEVX58Svnw2Yzi9RKR/5CYrCsSXaQ3pjOLAEFe4yHYSkVXySGnYvCoCWw9E1CAx2/S6cCZdkGCe
+vEsXCS+0yx5DaMkHJ8HSXPfqIbloEpw8nL+e/IBcm2PN7EeqJSdnoDfzAIJ9VNep+OkuE6N36B9K
+-----END CERTIFICATE-----" >> $ROOT_CA_PEM_FILE_PATH
 
 
-echo "Configuring control_proxy.yml file"
-echo ""
-echo "Please make sure you copied rootCA.pem, gateway.crt and gateway.key at below location"
-echo "rootca_cert: /var/opt/magma/tmp/certs/rootCA.pem"
-echo "gateway_cert: /var/opt/magma/certs/gateway.crt"
-echo "gateway_key: /var/opt/magma/certs/gateway.key"
-echo ""
-
-echo "Enter local port"
-read local_port
-
-echo "Enter cloud address"
-read cloud_address
-
-echo "Enter cloud port"
-read cloud_port
-
-echo "Enter bootstrap address"
-read bootstrap_address
-
-echo "Enter bootstrap port"
-read bootstrap_port
-
-echo "Enter fluentd address"
-read fluentd_address
-
-echo "Enter fluentd port"
-read fluentd_port
 
 while true; do
     read -p "Do you want to contiue with post installation ?" yn
     case $yn in
-        [Yy]* ) create_control_proxy_yml_file; echo "Running Post Installation Script"; /bin/bash /root/agw_post_install_ubuntu.sh;break;;
+        [Yy]* ) echo "Running Post Installation Script"; /bin/bash /root/agw_post_install_ubuntu.sh;break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
